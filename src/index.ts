19fb56454960586984c1ddcd41791139a9a7f637
Plasmatium <slv309@163.com>
2017-11-28 15:56:04 +0800

    mock fetching with delay implemented

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,43 @@
 // import axios from 'axios'
-
 export const testFunc = (n1: number, n2: number) => {
   return n1*2 + n2*3
 }
+
+export const mockFetch = (ctrl: number = 0.5) => {
+  let rand = Math.random()
+  let pass = rand >= ctrl
+  let timeout = rand*1000 + 2000 // 0.5 ~ 1.5 sec
+  return new Promise<string>((res, rej) => {
+    setTimeout(() => {
+      if (pass) {
+        res(`delayed (${timeout})ms`)
+      } else {
+        rej(`not passed: (${rand} < ${ctrl})`)
+      }
+    }, timeout)
+  })
+}
+
+export const testAsync = async () => {
+  console.log('start')
+  let rslt: string | void = 'ununundefined'
+  // try {
+  //   rslt = await delay(0.5)
+  // } catch (err) {
+  //   console.log('catched err:', err)
+  //   rslt = 'this is an error: ' + err
+  // }
+  rslt = await mockFetch(0.9).catch(rejReason => {
+    return rejReason
+  })
+  return rslt
+}
+
+export const testAsync2 = async () => {
+  let a = await testAsync().catch()
+  return a
+}
+
+testAsync2().then(value => {
+  console.log(`${value}\n`)
+}).catch(rej => console.log('last:', rej))
--- test/test_index.ts
+++ test/test_index.ts
@@ -1,10 +1,13 @@
 import * as chai from 'chai'
-import {testFunc} from '../src/index'
+import {testFunc, mockFetch} from '../src/index'
 
 const should = chai.should()
 
 describe('testFunc', () => {
   it('should be a function', () => {
     testFunc.should.be.a('function')
+    let x = 1
+    x.should.be.a('number')
+    x.should.be.an('number')
   })
 })
